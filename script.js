1580ec831c6e9c2825e0d9d7ae18543f1e679de2
vlad-yelnikov <vlad.yelnikov@gmail.com>
2019-06-26 19:49:27 +0300

    add clock-color

---
+++ script.js
@@ -0,0 +1,29 @@
+let clock = document.getElementById ('clock');
+let color = document.getElementById ('color');
+
+function hexaClock() {
+    let time = new Date();
+    let h = time.getHours().toString();
+    let m = time.getMinutes().toString();
+    let s = time.getSeconds().toString();
+
+    if (h.length < 2) {
+        h = '0' + h;
+    }
+    if (m.length < 2) {
+        m = '0' + m;
+    }
+    if (s.length < 2) {
+        s = '0' + s;
+    }  
+
+    let clockString = h + ':' + m + ':' + s;
+    let colorString = '#' +  h + m + s;
+
+    clock.textContent = clockString;
+    color.textContent = colorString;
+
+    document.body.style.background = colorString;
+}
+hexaClock();
+setInterval(hexaClock, 1000);
